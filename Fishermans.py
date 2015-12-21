b06e1503272ec91fd814bb423e785c1cf175b2fa
jenhayes <jeneve.hayes@gmail.com>
2015-12-21 02:11:00 +0000

    working code for python learning adventure game

---
+++ Fishermans.py
@@ -0,0 +1,57 @@
+import time
+
+#New function for Intro to Game
+
+def intro_chat():
+    #Arriving in SF
+
+    print "Welcome to San Francisco! I'm Py!"
+    time.sleep(2)
+    print "Today you will enjoy some fun and frivolity at the city's most popular sites!" 
+    time.sleep(2)
+    print "San Francisco is one of the most tech savvy destinations in the country, and as a result, people have some very strange quirks!"
+    time.sleep(3)
+    print "SFers are picky about how they communicate, which you will learn soon enough."
+    time.sleep(2)
+    print "In order to speak with many of the locals, you will need to learn 'Parseltongue', a finicky language if there ever were one."
+    time.sleep(3)
+    print "Don't worry, you'll learn as you go, and I can try to help when necessary!"
+    time.sleep(2)
+    print "First stop: Fisherman's Wharf for a bite to eat!"
+    time.sleep(2)
+
+intro_chat()
+
+#On to Fisherman's Wharf to a tasty Seafood Restaurant, called Merv's Mackrel and More!
+
+def mervs_mack():
+
+    #Arrive at FMW and MMM
+    print "You and your guide buddy arrive at little more than a hole in the Pier called 'MMM', which gives you hope for the tasty treats inside!"
+    time.sleep(3)
+    print "Your mouth waters a bit and you take a step inside, where you smell butter, fried foods, and happiness!"
+    time.sleep(2)
+    print "Py says:"
+    time.sleep(1)
+    print "Here we are at Merv's Mackerel and More! Merv has some of the tastiest seafood in SF, let's see what he's got cooking today!"
+    time.sleep(2)
+    
+    #Introduce Merv
+    print "A man with a burly beard and dirty apron walks up to you and Py, and introduces himself as Merv." 
+    time.sleep(1)
+    print "He seats you at a booth, and says that your server will be over in a moment."
+    
+    #Server and learning about printing and types
+    print "Hey ya'll! My name is Richie, one thing I need to say up front is, I only understand Parseltongue, so here are the rules!"
+    time.sleep(2)
+    print "In order to communicate with me, you will have to print your answers to me! This way I will be able to hear them and respond."
+    time.sleep(2)
+    print "To print, you will simply type print and then type what you would like to say!"
+    time.sleep(1)
+    print "It will look like this: " 
+    time.sleep(1)
+    print "print 'Hello Richie'" 
+    time.sleep(1)
+    print "or: print \"Hello Richie\"" 
+    
+mervs_mack()
